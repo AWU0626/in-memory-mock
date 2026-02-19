98d5d5b641d9fa7cb4e027b26f7c242edbcf6490
Aaron Wu <aaronwu321@gmail.com>
2026-02-18 20:04:30 -0500

    initializer template

---
+++ src/main/java/com/example/in_memory_mock/InMemoryMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.in_memory_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/in_memory_mock/InMemoryMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.in_memory_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
